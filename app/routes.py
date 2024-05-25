8bced7e815b123a78dde2264e0e49bdc14fb22e7
AdrianaRosedh <adriana.rose.dh@gmail.com>
2024-05-25 15:56:05 +0200

    dinamic routing

--- app/routes.py
+++ app/routes.py
@@ -1,75 +1,67 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, send_from_directory
+import os
 
 bp = Blueprint('main', __name__)
 
+def detect_language():
+    # Detect language from Accept-Language header
+    languages = request.headers.get('Accept-Language', '').split(',')
+    for lang in languages:
+        if lang.startswith('es'):
+            return 'es'
+    return 'en'
+
 @bp.route('/')
 def index():
-    current_language = session.get('language', 'en')
-    if current_language == 'es':
-        return redirect(url_for('main.index_es'))
-    return redirect(url_for('main.index_en'))
+    if 'language' not in session:
+        session['language'] = detect_language()
+    return redirect(url_for('main.index_localized', lang_code=session.get('language', 'en')))
 
-@bp.route('/en')
-def index_en():
-    session['language'] = 'en'
+@bp.route('/<lang_code>')
+def index_localized(lang_code):
+    session['language'] = lang_code
     email = "info@myahotelboutique.com"
     phone = "+52 (646) 388-2369"
+    if lang_code == 'es':
+        return render_template('index_es.html', email=email, phone=phone)
     return render_template('index_en.html', email=email, phone=phone)
-
-@bp.route('/es')
-def index_es():
-    session['language'] = 'es'
-    email = "info@myahotelboutique.com"
-    phone = "+52 (646) 388-2369"
-    return render_template('index_es.html', email=email, phone=phone)
 
 @bp.route('/set_language', methods=['POST'])
 def set_language():
     selected_language = request.form.get('language')
     current_url = request.form.get('current_url')
-    
+
     session['language'] = selected_language
-    
-    # Remove the leading language part from the URL
-    if current_url.startswith('/es'):
+
+    if current_url.startswith('/es/'):
         current_url = current_url[3:]
-    elif current_url.startswith('/en'):
+    elif current_url.startswith('/en/'):
         current_url = current_url[3:]
-    
-    # Prepend the new language to the URL
-    if selected_language == 'es':
-        new_url = f'/es{current_url}'
-    else:
-        new_url = f'/en{current_url}'
+    elif current_url in ['/es', '/en']:
+        current_url = '/'
+
+    new_url = f'/{selected_language}{current_url}' if current_url != '/' else f'/{selected_language}'
 
     return redirect(new_url)
 
-@bp.route('/oliveafarmtotable')
-def oliveafarmtotable():
-    current_language = session.get('language', 'en')
+@bp.route('/<lang_code>/oliveafarmtotable')
+def oliveafarmtotable(lang_code):
+    session['language'] = lang_code
     email = "olivea@myahotelboutique.com"
     phone = "+52 (646) 388-2369"
-    if current_language == 'es':
+    if lang_code == 'es':
         return render_template('oliveafarmtotable_es.html', email=email, phone=phone)
     return render_template('oliveafarmtotable_en.html', email=email, phone=phone)
 
-@bp.route('/es/oliveafarmtotable')
-def oliveafarmtotable_es():
-    email = "olivea@myahotelboutique.com"
-    phone = "+52 (646) 388-2369"
-    return render_template('oliveafarmtotable_es.html', email=email, phone=phone)
-
-@bp.route('/divino')
-def divino():
-    current_language = session.get('language', 'en')
+@bp.route('/<lang_code>/divino')
+def divino(lang_code):
+    session['language'] = lang_code
     email = "padel@myahotelboutique.com"
     phone = "+52 (646) 388-2369"
-    if current_language == 'es':
+    if lang_code == 'es':
         return render_template('divino_es.html', email=email, phone=phone)
     return render_template('divino_en.html', email=email, phone=phone)
 
-@bp.route('/es/divino')
-def divino_es():
-    email = "padel@myahotelboutique.com"
-    phone = "+52 (646) 388-2369"
-    return render_template('divino_es.html', email=email, phone=phone)
+@bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(bp.root_path, 'static', 'img', 'favicons'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
