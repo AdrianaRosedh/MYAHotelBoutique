1c369b2ee8d6c9f8e0af6a91c0c21b96259d8799
Adriana Rose <adriana.rose.dh@gmail.com>
2024-06-30 12:53:33 -0700

    trying to fix send-buttom-swal with touch

--- app/static/src/js/custom/chatbot.js
+++ app/static/src/js/custom/chatbot.js
@@ -9,9 +9,10 @@ function initChatbot() {
             sendMessage(event);
         }
     });
-    document.querySelector('.send-button').addEventListener('click', function(event) {
-        sendMessage(event);
-    });
+
+    const sendButton = document.querySelector('.send-button');
+    sendButton.addEventListener('click', sendMessage);
+    sendButton.addEventListener('touchstart', sendMessage);
 
     window.addEventListener('resize', handleResize);
     handleResize(); // Ensure proper styling on load
@@ -135,7 +136,8 @@ function openChatbotMobile() {
             });
 
             const sendButton = document.getElementById('send-button-swal');
-            sendButton.addEventListener('click', (event) => sendMessageSwal(event));
+            sendButton.addEventListener('click', sendMessageSwal);
+            sendButton.addEventListener('touchstart', sendMessageSwal);
 
             console.log("Event listeners attached");
         }
