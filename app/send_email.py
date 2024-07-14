d07c4eb27b082e5770e96d2fa6b3ec45e059b555
Adriana Rose <adriana.rose.dh@gmail.com>
2024-07-14 16:33:30 -0700

    trying to fix routes for lunch

--- app/send_email.py
+++ app/send_email.py
@@ -1,12 +1,46 @@
 # app/send_email.py
 import os
+import pickle
 import base64
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from app.get_credentials import get_credentials  # Ensure this matches your project structure
+from dotenv import load_dotenv, find_dotenv
 
+# Load environment variables from .env file
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+# Ensure environment variables are loaded
+SCOPES = os.getenv('GMAIL_SCOPES').split(',')
+CLIENT_SECRET_FILE = 'client_secret.json'
+TOKEN_PICKLE_FILE = 'token.pickle'
+
+# Decode and save the client secret json file
+client_secret_base64 = os.getenv('GMAIL_CLIENT_SECRET_BASE64')
+if client_secret_base64:
+    with open(CLIENT_SECRET_FILE, 'wb') as f:
+        f.write(base64.b64decode(client_secret_base64))
+
+# Decode and save the token pickle file
+token_pickle_base64 = os.getenv('TOKEN_PICKLE_BASE64')
+if token_pickle_base64:
+    with open(TOKEN_PICKLE_FILE, 'wb') as f:
+        f.write(base64.b64decode(token_pickle_base64))
 
 def get_gmail_service():
-    creds = get_credentials()
+    creds = None
+    if os.path.exists(TOKEN_PICKLE_FILE):
+        with open(TOKEN_PICKLE_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN_PICKLE_FILE, 'wb') as token:
+            pickle.dump(creds, token)
     service = build('gmail', 'v1', credentials=creds)
     return service
 
